34d8a9b988b7f09f3346ef74039a2c2db7f9432f
miyakei1225 <miyakeionigiri@yahoo.co.jp>
2023-01-08 02:13:20 +0900

    Add: Yup validation and appear validation message

--- native-front/src/screens/FavorRegisterScreen.tsx
+++ native-front/src/screens/FavorRegisterScreen.tsx
@@ -4,38 +4,41 @@ import { Button, TextInput, View } from 'react-native'
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 
+const schema = Yup.object().shape({
+  userName: Yup.string().max(15, '15文字以下で入力してください').required('Required'),
+  email: Yup.string().email('Invalid email address').required('Required')
+})
+
 export default function FavorRegisterScreen({ navigation }: RootStackScreenProps<'FavorRegister'>) {
   return (
     <ScrollView w={['100%', '100%']} h="100">
       <Box bgColor="white">
         <View>
           <Text>登録ページ</Text>
-          {
-            // validationSchema={Yup.object({
-            //   firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
-            //   lastName: Yup.string().max(20, 'Must be 20 characters or less').required('Required'),
-            //   email: Yup.string().email('Invalid email address').required('Required')
-            // }
-          }
           <Formik
-            initialValues={{ email: '', password: '' }}
+            initialValues={{ userName: '', email: '', password: '' }}
             onSubmit={(values) => console.log(values)}
-            // validationSchema={Yup.object({
-            //   firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
-            //   lastName: Yup.string().max(20, 'Must be 20 characters or less').required('Required'),
-            //   email: Yup.string().email('Invalid email address').required('Required')
-            // }
+            validationSchema={schema}
           >
-            {({ handleChange, handleBlur, handleSubmit, values }) => (
+            {({ handleChange, handleBlur, handleSubmit, isValid, isSubmitting, values, errors, touched }) => (
               <View>
+                <Text>ユーザー名:</Text>
+                <TextInput
+                  onChangeText={handleChange('userName')}
+                  onBlur={handleBlur('userName')}
+                  value={values.userName}
+                />
+                {errors.userName && touched.userName ? <Text>{errors.userName}</Text> : null}
                 <Text>Email:</Text>
                 <TextInput onChangeText={handleChange('email')} onBlur={handleBlur('email')} value={values.email} />
+                {errors.email && touched.email ? <Text>{errors.email}</Text> : null}
                 <Text>Password:</Text>
                 <TextInput
                   onChangeText={handleChange('password')}
                   onBlur={handleBlur('password')}
                   value={values.password}
                 />
+                {errors.password && touched.password ? <Text>{errors.password}</Text> : null}
                 {/* @ts-ignore */}
                 <Button onPress={handleSubmit} title="Submit" />
               </View>
