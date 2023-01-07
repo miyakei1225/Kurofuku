9a4fcfe40c7d91aec2a2505edc1323fa38b9a579
miyakei1225 <miyakeionigiri@yahoo.co.jp>
2023-01-07 23:18:16 +0900

    Add: formik and yup

--- native-front/src/screens/FavorRegisterScreen.tsx
+++ native-front/src/screens/FavorRegisterScreen.tsx
@@ -1,6 +1,8 @@
 import { RootStackScreenProps } from '../../types'
-import { Box, FormControl, Input, ScrollView, Stack, Text } from 'native-base'
-import { View } from 'react-native'
+import { Box, ScrollView, Text } from 'native-base'
+import { Button, TextInput, View } from 'react-native'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 export default function FavorRegisterScreen({ navigation }: RootStackScreenProps<'FavorRegister'>) {
   return (
@@ -8,18 +10,37 @@ export default function FavorRegisterScreen({ navigation }: RootStackScreenProps
       <Box bgColor="white">
         <View>
           <Text>登録ページ</Text>
-          <FormControl>
-            <Stack space={5}>
-              <Stack>
-                <FormControl.Label>Username</FormControl.Label>
-                <Input variant="underlined" p={2} placeholder="Username" />
-              </Stack>
-              <Stack>
-                <FormControl.Label>Password</FormControl.Label>
-                <Input variant="underlined" p={2} placeholder="Password" />
-              </Stack>
-            </Stack>
-          </FormControl>
+          {
+            // validationSchema={Yup.object({
+            //   firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+            //   lastName: Yup.string().max(20, 'Must be 20 characters or less').required('Required'),
+            //   email: Yup.string().email('Invalid email address').required('Required')
+            // }
+          }
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={(values) => console.log(values)}
+            // validationSchema={Yup.object({
+            //   firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+            //   lastName: Yup.string().max(20, 'Must be 20 characters or less').required('Required'),
+            //   email: Yup.string().email('Invalid email address').required('Required')
+            // }
+          >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+              <View>
+                <Text>Email:</Text>
+                <TextInput onChangeText={handleChange('email')} onBlur={handleBlur('email')} value={values.email} />
+                <Text>Password:</Text>
+                <TextInput
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                />
+                {/* @ts-ignore */}
+                <Button onPress={handleSubmit} title="Submit" />
+              </View>
+            )}
+          </Formik>
         </View>
       </Box>
     </ScrollView>
